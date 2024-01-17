c57b3d6ad85cfb9f8f621b31f0b20254f15fe0b5
KristiinaRaeste <129388937+KristiinaRaeste@users.noreply.github.com>
2024-01-17 19:41:58 +0200

    Create refresh.sh

---
+++ refresh.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Define the input file
+INFILE=pages_v1.txt
+
+# Make empty dir for new docs
+DIR=v_$1/md
+if [ ! -d ${DIR} ] 
+  mkdir ${DIR}
+fi
+
+# Download files from EE and NIIS repo
+
+IFS=$'\n' # set the Internal Field Separator to newline
+for LINE in $(cat "$INFILE")
+do
+    niis_url=https://raw.githubusercontent.com/nordic-institute/X-Road/${1}/doc/${LINE}.md
+    wget -A md ${niis_url} -P ${DIR}
+done
+
+## Fixs path's to be suitable for EE repo
+
+for LINE in $(cat "$INFILE")
+do
+    file_name=${LINE##*/}
+    for PATTERN in $(cat "$INFILE")
+    do
+      pattern_name=${PATTERN##*/}
+      sed -i "s@(.*$pattern_name.md)@($pattern_name.md)@g" ${DIR}/${file_name}.md
+    done
+done
