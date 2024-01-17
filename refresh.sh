be0e99afda1986e9ee84d604802a65cc3c1df3fe
KristiinaRaeste <129388937+KristiinaRaeste@users.noreply.github.com>
2024-01-17 21:41:28 +0200

    Update refresh.sh

--- refresh.sh
+++ refresh.sh
@@ -11,6 +11,7 @@ then
 fi
 
 # Download files from EE and NIIS repo
+echo "Downloading files from NIIS"
 
 IFS=$'\n' # set the Internal Field Separator to newline
 for LINE in $(cat "$INFILE")
@@ -19,6 +20,8 @@ do
     wget -A md ${niis_url} -P ${DIR}
 done
 
+echo "Files downloaded from NIIS"
+echo "Link fixes"
 ## Fixs path's to be suitable for EE repo
 
 for LINE in $(cat "$INFILE")
@@ -30,7 +33,8 @@ do
       sed -i "s@(.*$pattern_name.md)@($pattern_name.md)@g" ${DIR}/${file_name}.md
     done
 done
-
+echo "Link fixed"
+echo "Version differences"
 ## Prev and new version  diff
 for LINE in $(cat "$INFILE")
 do
