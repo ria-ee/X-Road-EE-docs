c1a1765ff8e55da3193981a12ae02acb5fa9e9ce
KristiinaRaeste <129388937+KristiinaRaeste@users.noreply.github.com>
2024-01-18 15:23:06 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,5 +7,5 @@ for LINE in $(cat "$INFILE")
 do
     file_name=${LINE##*/}
     echo "********************************************************"
-    test -f md/${file_name}.md && echo "old ${file_name}.md exists." || echo "old ${file_name}.md does not exist."; test -f ${DIR}/${file_name}.md && echo "niis ${file_name}.md exists." || echo "niis ${file_name}.md does not exist."
+    test -f md/${file_name}.md && echo "old ${file_name}.md OK" || echo "old ${file_name}.md ERROR"; test -f ${DIR}/${file_name}.md && echo "niis ${file_name}.md OK" || echo "niis ${file_name}.md ERROR"
 done
