eed891bb162d97732b0b81b761d8440d5a41ae8a
KristiinaRaeste <129388937+KristiinaRaeste@users.noreply.github.com>
2024-01-22 15:41:57 +0200

    Update refresh.sh

--- refresh.sh
+++ refresh.sh
@@ -8,6 +8,7 @@ DIR=v_$1
 
 RED="\e[31m"
 GREEN="\e[32m"
+YELLOW="\e33m"
 ENDCOLOR="\e[0m"
 # Download files from EE and NIIS repo
 echo "Downloading files from NIIS"
@@ -34,12 +35,13 @@ do
         do
           pattern_name=${PATTERN##*/}
           sed -i "s@(.*$pattern_name.md)@($pattern_name.md)@g" ${DIR}/${file_name}.md
+          echo -e "${YELLOW} ${file_name}.md changed ${YELLOW}"
         done
     fi
 done
 
 truncate -s 0 diff_$1.md
-
+echo "Files that are not automatically refreshed"
 x=1
 while [ $x -le 2 ]
 do
@@ -53,9 +55,10 @@ do
     file_name=${LINE##*/}
     if cmp -s "md/${file_name}.md" "${DIR}/${file_name}.md"
     then
-        echo "${file_name}.md didn't change"
+        echo -e "${GREEN} ${file_name}.md didn't change ${ENDCOLOR}"
     else
-        diff -a -y --suppress-common-lines <(echo ${file_name}: *NIIS*; cat ${DIR}/${file_name}.md) <(echo *EE*; cat md/${file_name}.md) | sed 2i============================================================= >> diff_$1.md
+        echo -e "${YELLOW} ${file_name}.md didn't change ${YELLOW}"
+        diff -a -y --suppress-common-lines <(echo ${file_name}: *NIIS*; cat ${DIR}/${file_name}.md) <(echo *EE*; cat md/${file_name}.md) | sed 2i========================================================= >> diff_$1.md
     fi
 done
 
