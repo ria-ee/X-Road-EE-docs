682b0f71ae876918296bfdfb56bcd4a306703a3a
KristiinaRaeste <kristiina.raeste@ria.ee>
2024-02-16 12:41:49 +0200

    workflow fix

--- automatics/clean.sh
+++ automatics/clean.sh
@@ -1,1 +1,9 @@
-echo $1
+if [ $1 != $2 ]
+    then
+        rm -rf md_prev
+        mv -f ../md md_prev
+        rm -rf v_$2
+        mv version.txt prev_version.txt
+        echo ${GITHUB_REF##*/} > version.txt
+fi
+    
