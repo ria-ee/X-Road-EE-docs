b739037883431cd2fde65f9bd2bf83f20c1f171c
KristiinaRaeste <129388937+KristiinaRaeste@users.noreply.github.com>
2024-02-26 10:27:53 +0200

    Update clean.sh

--- automatics/clean.sh
+++ automatics/clean.sh
@@ -4,7 +4,7 @@ if [[ $1 != $2 && $1 != "main" ]]
     then
         rm -rf md_prev/*
         cp ../md/*.md md_prev
-        rm -rf v_$(cat prev_version.txt)
+        rm -rf v_7.2.2
         echo v_$(cat prev_version.txt)
         mv version.txt prev_version.txt
         echo $1 > version.txt
