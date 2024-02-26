40c9edd7b0d668a52c853dcfa4c23a00cc7f3db8
KristiinaRaeste <129388937+KristiinaRaeste@users.noreply.github.com>
2024-02-26 12:42:12 +0200

    Update clean.sh

--- automatics/clean.sh
+++ automatics/clean.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
-which bash
+#!/usr/bin/bash
+
 if [[ $1 != $2 && $1 != "main" ]]
     then
         rm -rf md_prev/*
         cp ../md/*.md md_prev
-        find . -type d -name v_"$(cat prev_version.txt)" -exec rm -rfv "{}" \;
+        rm -rfv v_"$(cat prev_version.txt)"
         echo v_"$(cat prev_version.txt)"
         mv version.txt prev_version.txt
         echo $1 > version.txt
