52889cfd76df606f54d705f9719a92bf3360f257
KristiinaRaeste <129388937+KristiinaRaeste@users.noreply.github.com>
2024-02-26 11:48:45 +0200

    Update clean.sh

--- automatics/clean.sh
+++ automatics/clean.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+old_dir = v_"$(cat prev_version.txt)"
 if [[ $1 != $2 && $1 != "main" ]]
     then
         rm -rf md_prev/*
         cp ../md/*.md md_prev
-        find . -type d -name v_"$(cat prev_version.txt)" -exec rm -rf "{}" \;
+        find . -type d -name $old_dir -exec rm -rf "{}" \;
         echo v_"$(cat prev_version.txt)"
         mv version.txt prev_version.txt
         echo $1 > version.txt
